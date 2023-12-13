d1e7b13bb8c8078045916a7ff3b78ed961b13f98
Jakob Fender <jakob.fender@stud.hslu.ch>
2023-12-13 05:47:50 +0100

    make data story more focused on results

--- helper.py
+++ helper.py
@@ -1,10 +1,35 @@
+import numpy as np
 import pandas as pd
 
-custom_color_scale = [
-    (0.0, "red"),  # Low scores in red
-    (0.5, "orange"),  # Medium scores in orange
-    (1.0, "green")  # High scores in green
-]
+continent_color_map = {
+    'Europe': '#1f77b4',
+    'Asia': '#ff7f0e',
+    'Oceania': '#2ca02c',
+    'Americas': '#d62728',
+    'Africa': '#9467bd'
+}
+
+region_color_map = {
+    'Europe': '#1f77b4',
+    'Asia': '#ff7f0e',
+    'Oceania': '#2ca02c',
+    'Americas': '#d62728',
+    'Africa': '#9467bd',
+    'Northern Europe': '#1f77b4',
+    'Eastern Europe': '#1f77b4',
+    'Southern Europe': '#1f77b4',
+    'Western Europe': '#1f77b4',
+    'Eastern Asia': '#ff7f0e',
+    'Central Asia': '#ff7f0e',
+    'South-eastern Asia': '#ff7f0e',
+    'Southern Asia': '#ff7f0e',
+    'Western Asia': '#ff7f0e',
+    'Australia and New Zealand': '#2ca02c',
+    'Northern America': '#d62728',
+    'Latin America and the Caribbean': '#d62728',
+    'Northern Africa': '#9467bd',
+    'Sub-Saharan Africa': '#9467bd',
+}
 
 # Import data
 whr23 = pd.read_csv('data/WHR2023.csv')
@@ -18,6 +43,18 @@ continent_file = pd.read_csv('data/continents2.csv')
 continent_file.rename(columns={'name': 'Country name', 'alpha-2': 'country code'}, inplace=True)
 column_order = ['Country name', 'region', 'sub-region', 'country code']
 
+# Renaming countries to match the main dataset
+whr23.loc[whr23['Country name'] == 'Congo (Kinshasa)', 'Country name'] = 'Congo (Democratic Republic Of The)'
+whr23.loc[whr23['Country name'] == 'Congo (Brazzaville)', 'Country name'] = 'Congo'
+whr23.loc[whr23['Country name'] == 'Turkiye', 'Country name'] = 'Turkey'
+whr23.loc[whr23['Country name'] == 'Czechia', 'Country name'] = 'Czech Republic'
+whr23.loc[whr23['Country name'] == 'Taiwan Province of China', 'Country name'] = 'Taiwan'
+whr23.loc[whr23['Country name'] == 'Bosnia and Herzegovina', 'Country name'] = 'Bosnia And Herzegovina'
+whr23.loc[whr23['Country name'] == 'Hong Kong S.A.R. of China', 'Country name'] = 'Hong Kong'
+whr23.loc[whr23['Country name'] == 'North Macedonia', 'Country name'] = 'Macedonia'
+whr23.loc[whr23['Country name'] == 'State of Palestine', 'Country name'] = 'Palestine, State of'
+whr23.loc[whr23['Country name'] == 'Ivory Coast', 'Country name'] = 'Côte D\'Ivoire'
+
 # Merge region and sub-region into whr23score after country name
 merged_data = whr23.merge(continent_file[['Country name', 'region', 'sub-region', 'country code']], on='Country name')
 column_order = ['Country name', 'region', 'sub-region', 'country code'] + [col for col in merged_data.columns if
@@ -26,6 +63,17 @@ merged_data = merged_data[column_order]
 
 # Rename the 'Country' column to 'Country name' in the extra_data DataFrame
 world_data.rename(columns={'Country': 'Country name'}, inplace=True)
+
+# Change names of countries to match the main dataset
+merged_data.loc[merged_data['Country name'] == 'Ireland', 'Country name'] = 'Republic of Ireland'
+merged_data.loc[merged_data['Country name'] == 'Bosnia And Herzegovina', 'Country name'] = 'Bosnia and Herzegovina'
+merged_data.loc[merged_data['Country name'] == 'Congo', 'Country name'] = 'Republic of the Congo'
+merged_data.loc[merged_data[
+                    'Country name'] == 'Congo (Democratic Republic Of The)', 'Country name'] = 'Democratic Republic of the Congo'
+merged_data.loc[merged_data['Country name'] == 'Macedonia', 'Country name'] = 'North Macedonia'
+merged_data.loc[merged_data['Country name'] == 'Côte D\'Ivoire', 'Country name'] = 'Ivory Coast'
+merged_data.loc[merged_data['Country name'] == 'Palestine, State of', 'Country name'] = 'Palestinian National Authority'
+merged_data.loc[merged_data['Country name'] == 'Gambia', 'Country name'] = 'The Gambia'
 
 # Change Unenployment rate to a float
 world_data['Unemployment rate'] = world_data['Unemployment rate'].str.replace('%', '').astype(float)
@@ -39,3 +87,19 @@ full_data = merged_data.merge(
 full_data['Urban_population'] = full_data['Urban_population'].str.replace(',', '').astype(float)
 full_data['Population'] = full_data['Population'].str.replace(',', '').astype(float)
 full_data['Urban population percentage'] = (full_data['Urban_population'] / full_data['Population']) * 100
+full_data['Continent'] = full_data['region']
+full_data['Region'] = full_data['sub-region']
+full_data['Happiness Score'] = full_data['Ladder score']
+full_data.drop(['region', 'sub-region', 'Ladder score'], axis=1, inplace=True)
+
+# Normalize the data
+full_data["Normalized Logged GDP per capita"] = full_data["Logged GDP per capita"] / full_data["Logged GDP per capita"].max()
+full_data["Normalized Social support"] = full_data["Social support"] / full_data["Social support"].max()
+full_data["Normalized Healthy life expectancy"] = full_data["Healthy life expectancy"] / full_data["Healthy life expectancy"].max()
+full_data["Normalized Freedom to make life choices"] = full_data["Freedom to make life choices"] / full_data["Freedom to make life choices"].max()
+full_data["Normalized Happiness Score"] = full_data["Happiness Score"] / full_data["Happiness Score"].max()
+
+
+# Replace NaN values with the respective column mean
+numeric_means = full_data.select_dtypes(include=[np.number]).mean()
+full_data.update(full_data.select_dtypes(include=[np.number]).fillna(numeric_means))
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import plotly.subplots as sp
 import streamlit as st
 
-from helper import custom_color_scale, full_data
+from helper import full_data, continent_color_map, region_color_map
 
 st.set_page_config(layout='wide', page_title='Happiness & Economics', page_icon=':smiley:')
 custom_css = """
@@ -19,149 +19,199 @@ custom_css = """
 # Inject custom CSS with markdown
 st.markdown(custom_css, unsafe_allow_html=True)
 
+WIDE_CONTAINER_COLUMNS = [1, 5, 1]
+SMALL_CONTAINER_COLUMNS = [1, 3, 1]
+
 # Create a container
 container = st.container()
 
 # Create three columns
-_, wide_layout, _ = container.columns([1, 4, 1])  # The middle column has more space
+_, wide_layout, _ = container.columns(WIDE_CONTAINER_COLUMNS)  # The middle column has more space
 
 # Display the plot in the middle column
 with wide_layout:
     small_container = st.container()
-    _, small_layout, _ = small_container.columns([1, 2, 1])
+    _, small_layout, _ = small_container.columns(SMALL_CONTAINER_COLUMNS)
 
     with small_layout:
         """
         # WORK IN PROGRESS! The website is not finished yet!
-        # Happiness & Economics
+        # How the economy influences our happiness
+        #### Money can't buy happiness, but it makes living a lot easier. 
         ## Disclaimer
         This data story is a project developed as part of the Data Visualization course at Luzern University of Applied 
         Sciences and Arts. In the spirit of transparency we would like to disclose that some portions of the text have 
         been generated with the assistance of ChatGPT. Every text that has been generated with the assistance of
         ChatGPT have been either edited or completely rewritten by the authors of this data story.
             
-        The content of this data story is derived from a notebook authored by Lucy Allan. You can find her notebook and access the datasets she utilized at the following links:
+        The content of this data story is derived from a notebook authored by Lucy Allan. You can find her notebook and 
+        access the datasets she utilized at the following links:
         
         - [Lucy Allan's Notebook](https://www.kaggle.com/code/lucyallan/world-happiness-report-2023-data/notebook)
         - [World Happiness Report 2023](https://www.kaggle.com/datasets/ajaypalsinghlo/world-happiness-report-2023/)
         - [Global Country Information 2023](https://www.kaggle.com/datasets/nelgiriyewithana/countries-of-the-world-2023)
         - [Continent2](https://www.kaggle.com/datasets/semihizinli/continent2)
         
-        
+        Names of countries, and their borders are not intended to be a political statement. We are aware that some 
+        countries have disputed borders. We are using the data as it is provided by the datasets!
         
         ## Introduction
         
         In our quest to understand what influences happiness around the world, we embark on a data-driven story 
         that navigates through various indicators of well-being. With the aid of global data, we plot happiness 
-        scores against a myriad of metrics that will give us a better understanding of what makes us happy.
+        scores against a myriad of metrics that will give us a better understanding of what makes us happy. We will also
+        explore the geographic distribution of happiness scores and how they vary across continents. 
         """
 
         """
-        ### Happiness Score by Country
-        Here you can see the countries of the world and their happiness score. Some countries don't have a score and are
-        not marked on the map. This plot is interactive, so you can zoom in or hover over the countries to see their 
-        score.
+        ### Happiness, on a Map?
+        The World Happiness Report is a landmark survey of the state of global happiness. The survey is conducted by the
+        Gallup World Poll.  It surveys participants from 137 countries and ranks determines a happiness score for each 
+        country. The happiness score in a number between 0 and 10, with 10 being the happiest. Sadly not every country
+        is included in the survey. These countries have a white color on the map. 
+        
+        _Hint: You can change the regions that are displayed on the plot_
         """
 
-        min_score = st.slider('Minimum Happiness Score ', 0.0, 10.0, 0.0, 0.01)
+        with st.expander("Change Parameters"):
+            min_score = st.slider('Minimum Happiness Score ', 0.0, 10.0, 0.0, 0.01)
 
-        all_regions = full_data["region"].unique().tolist()
-        map_selected_regions = st.multiselect('Selected regions', ['All'] + all_regions, "All",
-                                              key="map_selected_regions")
+            all_regions = full_data["Continent"].unique().tolist()
+            map_selected_regions = st.multiselect('Selected regions', ['All'] + all_regions, "All",
+                                                  key="map_selected_regions")
 
-        if 'All' in map_selected_regions or map_selected_regions == []:
-            map_selected_regions = all_regions
+            if 'All' in map_selected_regions or map_selected_regions == []:
+                map_selected_regions = all_regions
 
-        map_displayed_data = full_data[(full_data['Ladder score'] > min_score)
-                                       & (full_data['region'].isin(map_selected_regions))]
+            map_displayed_data = full_data[(full_data['Happiness Score'] > min_score)
+                                           & (full_data['Continent'].isin(map_selected_regions))]
 
-    fig = px.choropleth(map_displayed_data, locations="Country name", locationmode='country names',
-                        color="Ladder score",
-                        hover_name="Country name",
-                        hover_data=['Ladder score'],
-                        color_continuous_scale=custom_color_scale,
-                        height=800)
+    happiness_map = px.choropleth(map_displayed_data,
+                                  locations="Country name",
+                                  locationmode='country names',
+                                  color="Happiness Score",
+                                  hover_name="Country name",
+                                  hover_data=['Happiness Score'],
+                                  color_continuous_scale='viridis',
+                                  height=800)
 
-    fig.update_layout(autosize=True, showlegend=False)
-    st.plotly_chart(fig, use_container_width=True)
+    happiness_map.update_geos(showocean=True, oceancolor="#0e1117")
+    happiness_map.update_layout(dragmode=False)
+    happiness_map.update_traces(marker_line_width=0)
+
+    st.plotly_chart(happiness_map, use_container_width=True)
 
     small_container = st.container()
-    _, small_layout, _ = small_container.columns([1, 2, 1])
+    _, small_layout, _ = small_container.columns(SMALL_CONTAINER_COLUMNS)
 
     with small_layout:
         """
         Something that you can see in the map is that the countries with the highest happiness score are in 
         Europe, North America and Oceania. The countries with the lowest happiness score are in Africa, the Middle
-        East and South Asia. Notable exceptions are Israel, Saudi Arabia and the United Arab Emirates, which have a
-        high happiness score. for a region that is otherwise not very happy.
-
-        ### Distribution of Happiness Scores by Continent
+        East and South Asia. 
+        
+        ### Does your continent influence your happiness?
         """
-        fig = px.box(full_data, x='region', y='Ladder score', color='region', height=600,
-                     labels={'region': 'Continent', 'Ladder score': 'Happiness Score'})
+        continent_order = {"Continent": full_data.groupby("Continent")["Happiness Score"].median()
+        .sort_values(ascending=False).index.tolist()}
 
-        fig.update_layout(xaxis_title='Continent', yaxis_title='Happiness Score')
+        fig = px.box(full_data, y='Continent', x='Happiness Score', color='Continent', height=600, orientation='h',
+                     color_discrete_map=continent_color_map, category_orders=continent_order,
+                     hover_data=['Country name'])
+
+        fig.update_layout(xaxis_title='Happiness Score', yaxis_title='Continent')
         fig.update_layout(autosize=True)
         st.plotly_chart(fig, use_container_width=True)
 
         """
-        The boxplot proves the point that we made earlier. With a median score of 6.4, a lower fence being at 5.0 and 
-        the upper fence at 7.8, Europe is the happiest continent. Asia is the continent with the largest spread of
-        happiness scores, ranging from 7.4 to 1.8. The median score for Asia is 5.4. Due to the low number of countries
-        in Oceanica, the boxplot is not very representative. The scores of the Americas are closer together and 
-        comparable to the scores of Europe. Africa has by far the lowest median score of 4.4 and the lowest upper fence
-        at 5.9.
+        Oceanica is by far the happiest continent. But this is not a fair comparison, because Oceanica only has 2
+        countries with data. This scews the data in their favor. The next best continent is Europe. With the highest
+        upper fence and the highest median score. The Americas are not that far off. With their upper and lower fences 
+        being very close to each other. Asia has the largest spread of happiness scores. Africa is the unhappiest
+        continent by far. Their median score is lower than the lower fence of Europe.
         
-        ### Distribution of Happiness Scores by Sub-Region        
+        ### Let's take an even closer look  
+        The following boxplot shows the happiness scores of the regions of a continent. The boxplots have been sorted
+        by their median score and colored by their continent. 
+        
+        _Hint: You can change the regions that are displayed on the plot_
         """
 
-        box_selected_regions = st.multiselect('Selected regions', all_regions + ['All']
-                                              , "All", key="box_selected_regions")
+        with st.expander("Change Parameters"):
+            box_selected_regions = st.multiselect('Selected regions', all_regions + ['All']
+                                                  , "All", key="box_selected_regions")
 
-        if 'All' in box_selected_regions or box_selected_regions == []:
-            box_selected_regions = all_regions
+            if 'All' in box_selected_regions or box_selected_regions == []:
+                box_selected_regions = all_regions
 
-        all_eligible_sub_regions = full_data[full_data["region"].isin(box_selected_regions)][
-            "sub-region"].unique().tolist()
-        box_selected_sub_regions = st.multiselect('Selected sub-regions', ['All'] + all_eligible_sub_regions,
-                                                  ["All"],
-                                                  key="box_selected_sub_regions")
+            all_eligible_sub_regions = full_data[full_data["Continent"].isin(box_selected_regions)][
+                "Region"].unique().tolist()
+            box_selected_sub_regions = st.multiselect('Selected sub-regions', ['All'] + all_eligible_sub_regions,
+                                                      ["All"],
+                                                      key="box_selected_sub_regions")
 
-        if 'All' in box_selected_sub_regions or box_selected_sub_regions == []:
-            box_selected_sub_regions = all_eligible_sub_regions
+            if 'All' in box_selected_sub_regions or box_selected_sub_regions == []:
+                box_selected_sub_regions = all_eligible_sub_regions
 
-        box_displayed_data = full_data[(full_data['Ladder score'] > min_score)
-                                       & (full_data['region'].isin(box_selected_regions)
-                                          & (full_data['sub-region'].isin(box_selected_sub_regions)))]
+            box_displayed_data = full_data[(full_data['Happiness Score'] > min_score)
+                                           & (full_data['Continent'].isin(box_selected_regions)
+                                              & (full_data['Region'].isin(box_selected_sub_regions)))]
 
-    fig = px.box(box_displayed_data, x='sub-region', y='Ladder score', color='sub-region', height=600,
-                 labels={'sub-region': 'Sub Region', 'Ladder score': 'Happiness Score'})
+    region_order = {"Region": box_displayed_data.groupby("Region")["Happiness Score"].median()
+    .sort_values(ascending=False).index.tolist()}
 
-    fig.update_layout(xaxis_title='Sub Region', yaxis_title='Happiness Score')
+    fig = px.box(box_displayed_data, y='Region', x='Happiness Score', color='Region', height=1200,
+                 orientation='h', color_discrete_map=region_color_map,
+                 category_orders=region_order, hover_data=['Country name'])
+
+    fig.update_layout(yaxis_title='Sub Region', xaxis_title='Happiness Score')
     fig.update_layout(autosize=True)
     st.plotly_chart(fig, use_container_width=True)
 
     small_container = st.container()
-    _, small_layout, _ = small_container.columns([1, 2, 1])
+    _, small_layout, _ = small_container.columns(SMALL_CONTAINER_COLUMNS)
 
     with small_layout:
         """
-        The boxplot shows that Western Europe is by far the happiest sub-region with a median score of 7.0. The
-        unhappiest sub-region is Sub-Saharan Africa with a median score of 4.4. If you want mess with the data yourself
-        you can select the regions and sub-regions that you want to see in the boxplot.
+        Something that is very easy to see is that the happiest regions are from Europe and North America. No big
+        surprise there. These regions perfectly match "The West". The most interesting region is by far West Asia. The 
+        happiness scores in this region varies from the second lowest to one of the highest.
+        
+                
+        ### 
+        You don't have to be an economist to see that the happiest countries are all developed countries. But do 
         """
+
+
+        # Calculate the average Happiness Score and Logged GDP by continent
+        avg_data = full_data.groupby('Continent')[['Normalized Happiness Score','Normalized Logged GDP per capita',
+                                                   'Normalized Social support','Normalized Healthy life expectancy',
+                                                   'Normalized Freedom to make life choices']].mean().reset_index()
+        # Create a grouped bar chart for normalized data
+        fig = px.bar(
+            avg_data.melt(id_vars='Continent', var_name='Metric', value_name='Percentage'),
+            y='Continent',
+            x='Percentage',
+            color='Metric',
+            barmode='group',
+            title='Normalized Metrics by Continent',
+            orientation='h',
+            height=800
+        ) # todo:  sort by happiness score, labels
+        # Show the combined bar chart
+        st.plotly_chart(fig, use_container_width=True)
 
     _, col1, col2, _ = st.columns(4)
     with col1:
         """
         ### Happiest Countries
         """
-        happiest_countries = full_data[['Country name', 'Ladder score']]
-        happiest_countries = happiest_countries.sort_values(by='Ladder score', ascending=False).head(5)
+        happiest_countries = full_data[['Country name', 'Happiness Score']]
+        happiest_countries = happiest_countries.sort_values(by='Happiness Score', ascending=False).head(5)
 
         formatted_happiest_countries = ""
         for index, row in happiest_countries.iterrows():
-            formatted_happiest_countries += f"{index + 1}. {row['Country name']} ({row['Ladder score']}) \n"
+            formatted_happiest_countries += f"{index + 1}. {row['Country name']} ({row['Happiness Score']}) \n"
 
         st.text(formatted_happiest_countries)
 
@@ -170,19 +220,19 @@ with wide_layout:
         """
         ### Unhappiest Countries
         """
-        unhappiest_countries = full_data[['Country name', 'Ladder score']]
-        unhappiest_countries = unhappiest_countries.sort_values(by='Ladder score', ascending=True).head(5)
+        unhappiest_countries = full_data[['Country name', 'Happiness Score']]
+        unhappiest_countries = unhappiest_countries.sort_values(by='Happiness Score', ascending=True).head(5)
 
         formatted_unhappiest_countries = ""
         rank = 128
         for index, row in unhappiest_countries.iterrows():
-            formatted_unhappiest_countries += f"{rank}. {row['Country name']} ({row['Ladder score']}) \n"
+            formatted_unhappiest_countries += f"{rank}. {row['Country name']} ({row['Happiness Score']}) \n"
             rank -= 1
 
         st.text(formatted_unhappiest_countries)
 
     small_container = st.container()
-    _, small_layout, _ = small_container.columns([1, 2, 1])
+    _, small_layout, _ = small_container.columns(SMALL_CONTAINER_COLUMNS)
 
     with small_layout:
         """
@@ -191,6 +241,7 @@ with wide_layout:
         other end of the spectrum, Afghanistan occupies the lowest position, ranking as the unhappiest country with a 
         significantly lower score of 1.859. Lebanon, Sierra Leone, Zimbabwe, and Botswana also find themselves among 
         the unhappiest nations, with scores ranging from 2.392 to 3.435.
+      
         
         
         ### Diverse Metrics and their Influence on Happiness
@@ -220,10 +271,10 @@ with wide_layout:
 
     # Add each scatter plot to the respective column in the subplot
     for i, x_var in enumerate(plots_details):
-        correlation = round(np.corrcoef(full_data['Ladder score'], full_data[x_var])[0, 1], 2)
+        correlation = round(np.corrcoef(full_data['Happiness Score'], full_data[x_var])[0, 1], 2)
 
-        scatter_plot = px.scatter(full_data, x=x_var, y='Ladder score', color='region',
-                                  hover_name='Country name', hover_data=['Ladder score', x_var])
+        scatter_plot = px.scatter(full_data, x=x_var, y='Happiness Score', color='Continent',
+                                  hover_name='Country name', hover_data=['Happiness Score', x_var])
 
         current_row = floor(i / num_cols) + 1
         current_col = i % num_cols + 1
@@ -245,7 +296,7 @@ with wide_layout:
     st.plotly_chart(fig, use_container_width=True)
 
     small_container = st.container()
-    _, small_layout, _ = small_container.columns([1, 2, 1])
+    _, small_layout, _ = small_container.columns(SMALL_CONTAINER_COLUMNS)
 
     with small_layout:
         """
